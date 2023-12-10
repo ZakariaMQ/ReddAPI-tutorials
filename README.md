be5a1b48f701007b478b46ec9a2874bc6b04156f
zakariaMQ <zm554765@gmail.com>
2023-12-10 10:04:52 +0100

    added Readme + requirements.txt + added docs into login.py

---
+++ README.md
@@ -0,0 +1,21 @@
+# ReddAPI-tutorials
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+
+## Description
+
+A full tutorial on how to use ReddAPI as a developer it contains documentation + all what you need to understand ReddAPI and start building your next Awesome SaaS project.
+
+
+
+## Installation
+
+you can install this repo using the three easy steps
+    ```bash
+    git clone https://github.com/ZakariaMQ/ReddAPI-tutorials.git
+    cd ReddAPI-tutorials
+    pip install resuirements.txt
+
+## a little Note
+To use all the code or ReddAPI you need first tio get your API key at [Rapidapi.com](https://rapidapi.com/SeasonedCode/api/reddapi)
+made with love by SeasonedCode from the BHW forum :)
--- python/login.py
+++ python/login.py
@@ -5,18 +5,18 @@ from creds import *
 url = "https://reddapi.p.rapidapi.com/api/login"
 
 payload = {
-	"username": username,
-	"password": password,
-	"useragent": useragent,
-	"proxy": proxy
+	"username": username,   # your Reddit account username
+	"password": password,   # your Reddit account password
+	"useragent": useragent, # your useragent
+	"proxy": proxy          #your proxy it should on the following format http://ip:port
 }
 
 headers = {
 	"content-type": "application/json",
-	"X-RapidAPI-Key": X_RapidAPI_Key,
+	"X-RapidAPI-Key": X_RapidAPI_Key,  # Get your API key at https://rapidapi.com/SeasonedCode/api/reddapi
 	"X-RapidAPI-Host": "reddapi.p.rapidapi.com"
 }
 
 response = requests.post(url, json=payload, headers=headers)
 
-print(response.text)
+print(response.json())
