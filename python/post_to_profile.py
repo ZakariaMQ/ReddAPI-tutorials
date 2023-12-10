7805f2251ca82d89d6863d42869a4ca0a580284c
zakariaMQ <zm554765@gmail.com>
2023-12-10 13:24:28 +0100

    added post_to_profile.py

---
+++ python/post_to_profile.py
@@ -0,0 +1,25 @@
+import requests
+
+from creds import *
+
+url = "https://reddapi.p.rapidapi.com/api/post_to_profile"
+
+payload = {
+	"useragent": useragent,  # your useragent
+	"proxy": proxy,          # your proxy it should on the following format http://ip:port
+	"bearer": bearer,        # for make the auth to reddit bearer is the token_v2 returned from the login endpoint",
+	"username": username,    # your Reddit account username
+	"title": title,          # the post title
+	"text": text,            # the post body
+	"is_nsfw": "either true or false" # true if it is nsfw false if it isn't nsfw
+}
+
+headers = {
+	"content-type": "application/json",
+	"X-RapidAPI-Key": X_RapidAPI_Key,  # Get your API key at https://rapidapi.com/SeasonedCode/api/reddapi
+	"X-RapidAPI-Host": "reddapi.p.rapidapi.com"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.json())
