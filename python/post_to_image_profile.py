5ded072bb226e15cfbb41a07d663dfe7920f7438
zakariaMQ <zm554765@gmail.com>
2024-08-16 09:02:15 +0100

    added "post_to_image_profile.py"

---
+++ python/post_to_image_profile.py
@@ -0,0 +1,23 @@
+import requests
+
+from creds import *
+
+url = "https://reddapi.p.rapidapi.com/api/post_to_profile/image"
+
+payload = {
+	"proxy": proxy,          # your proxy it should on the following format http://ip:port or http://user:pass@ip:port
+	"cookies": [cookies],    # list of cookies obtained from our login endpoint for authentication
+	"username": username,    # your Reddit account username
+	"title": title,          # the post title
+	"base64_images": ["base64 encoded image1", "base64 encoded image2"], # a list of image(s) encoded into base64
+	"is_nsfw": "either true or false" # true if it is nsfw false if it isn't nsfw
+}
+headers = {
+	"content-type": "application/json",
+	"X-RapidAPI-Key": X_RapidAPI_Key,  # Get your API key at https://rapidapi.com/SeasonedCode/api/reddapi
+	"X-RapidAPI-Host": "reddapi.p.rapidapi.com"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.json())
